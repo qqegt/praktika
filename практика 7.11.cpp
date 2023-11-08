5e12c8c2a88e225fb9586eedbb98de8d7198d0b3
qqegt <150118414+qqegt@users.noreply.github.com>
2023-11-08 14:02:13 +0300

    практика 7.11.cpp

--- практика 7.11.cpp
+++ практика 7.11.cpp
@@ -1,29 +1,32 @@
-﻿// ConsoleApplication15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// ConsoleApplication16.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include <iostream>
-
+using namespace std;
 int main() 
 {
-    using namespace std;
     setlocale(LC_ALL, "");
-    int num1, num2, userAnswer, correctAnswer;
 
-    
+    int num1, num2;
+
+    // Ввод двух однозначных чисел
     cout << "Введите первое однозначное число: ";
     cin >> num1;
 
     cout << "Введите второе однозначное число: ";
     cin >> num2;
 
-    correctAnswer = num1 * num2;
+    // Подсчет правильного ответа
+    int correctAnswer = num1 * num2;
 
-
+    // Вопрос пользователю
     cout << "Сколько будет " << num1 << " умножить на " << num2 << "? ";
+
+    int userAnswer;
     cin >> userAnswer;
 
+    // Проверка ответа пользователя
     if (userAnswer == correctAnswer) {
-        cout << "Правильно!" << std::endl;
+        cout << "Правильно! Ответ: " << correctAnswer << endl;
     }
     else {
         cout << "Неправильно. Правильный ответ: " << correctAnswer << endl;
