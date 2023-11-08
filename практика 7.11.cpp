daf288e2ca35a5c185d8e33e225c0d988b2327c0
qqegt <150118414+qqegt@users.noreply.github.com>
2023-11-08 13:43:46 +0300

    Add files via upload

---
+++ практика 7.11.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() 
+{
+    using namespace std;
+    setlocale(LC_ALL, "");
+    int num1, num2, userAnswer, correctAnswer;
+
+    
+    cout << "Введите первое однозначное число: ";
+    cin >> num1;
+
+    cout << "Введите второе однозначное число: ";
+    cin >> num2;
+
+    correctAnswer = num1 * num2;
+
+
+    cout << "Сколько будет " << num1 << " умножить на " << num2 << "? ";
+    cin >> userAnswer;
+
+    if (userAnswer == correctAnswer) {
+        cout << "Правильно!" << std::endl;
+    }
+    else {
+        cout << "Неправильно. Правильный ответ: " << correctAnswer << endl;
+    }
+
+    return 0;
+}
