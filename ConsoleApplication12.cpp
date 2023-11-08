6057fa4bffc7eb5b0891ec2dcdba7a1e087d146d
qqegt <150118414+qqegt@users.noreply.github.com>
2023-11-08 12:01:40 +0300

    Add files via upload

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,21 @@
+﻿// ConsoleApplication12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n;
+    std::cout << "Введите число n: ";
+    std::cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    std::cout << "Сумма чисел от 1 до " << n << " равна " << sum << std::endl;
+
+    return 0;
+}
